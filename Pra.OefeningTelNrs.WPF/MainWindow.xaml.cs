45b559f062febfa7810a9fb8aad5193b44a59f7f
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-09-28 12:55:29 +0200

    Add project files.

---
+++ Pra.OefeningTelNrs.WPF/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Pra.OefeningTelNrs.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        #region Global variables
+        #endregion
+        //======================
+        #region Window events
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+        }
+        #endregion
+        //======================
+        #region GUI stuff
+
+        #endregion
+
+        //======================
+        #region Event handlers
+
+
+        private void lstPhoneNumbers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnEndSearch_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        #endregion
+
+    }
+}
